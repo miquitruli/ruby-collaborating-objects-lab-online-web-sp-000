8b466831fad9c364de845c74860b19fb51197cb8
MICAELLA COLOMBO <micaelc@g.clemson.edu>
2020-08-03 23:50:33 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -20,7 +20,7 @@ class Song
 
   def self.new_by_filename(file)
     split_file = file.split(" - ")
-    song = Song.new(split_file[1])
+    song = self.new(split_file[1])
     song.artist = Artist.find_or_create_by_name(split_file[0])
     song
   end
