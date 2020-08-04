8fd021973b38b83e4f31e07ff3c4e0f6b263c9bc
MICAELLA COLOMBO <micaelc@g.clemson.edu>
2020-08-04 00:01:22 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,18 +1,15 @@
 class Song
-  attr_accessor :name, :artist
-
-
+  attr_accessor :name
+  attr_accessor :artist
 
   def initialize(name)
-    @name=name
+    @name = name
   end
 
-
-  def self.new_by_filename(file)
-    split_file = file.split(" - ")
-    song = self.new(split_file[1])
-    song.artist = Artist.find_or_create_by_name(split_file[0])
+  def self.new_by_filename(filename)
+    split_filename = filename.split(" - ")
+    song = Song.new(split_filename[1])
+    song.artist = Artist.find_or_create_by_name(split_filename[0])
     song
   end
-
 end
