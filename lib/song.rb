4c48959f48743c11b8a1c9dad1a247de58f5c875
MICAELLA COLOMBO <micaelc@g.clemson.edu>
2020-08-04 00:19:09 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -13,8 +13,8 @@ class Song
 
   def artist_name=(name)
     artist = Artist.find_or_create_by_name(name)
-    self.artist = artist
-    artist.add_song(self)
+    @@all.artist = artist
+    artist.add_song(song)
   end
 
   def self.new_by_filename(file)
