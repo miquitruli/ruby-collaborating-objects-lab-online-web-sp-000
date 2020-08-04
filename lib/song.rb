d2ac92afb3d06927f2fa51162c3bf0d16e676619
MICAELLA COLOMBO <micaelc@g.clemson.edu>
2020-08-04 00:35:39 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -11,10 +11,12 @@ class Song
     @@all << self
   end
 
+  def self.find_by_artist(artist)
+    Song.all.select {|song|song.artist == artist}
+  end
+
   def artist_name=(name)
-    artist = Artist.find_or_create_by_name(name)
-    self.artist = artist
-    artist.add_song(self)
+    self.artist = Artist.find_or_create_by_name(name)
   end
 
   def self.new_by_filename(file)
