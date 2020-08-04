631182587531a28beac4fd24689a3298a6f1c3d1
MICAELLA COLOMBO <micaelc@g.clemson.edu>
2020-08-04 00:02:30 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,42 +1,39 @@
 class Artist
   attr_accessor :name, :songs
-  @@all=[]
+  @@all = []
 
   def initialize(name)
-    @name=name
-    @songs=[]
-  end
-
-  def add_song(song)
-    @songs << song
-  end
-
-  def self.all
-    @@all
-  end
-
-  def self.find_or_create_by_name(name)
-    artist = @@all.find {|artist| artist.name == name}
-    if artist==nil
-      artist = Artist.new(name)
-      artist.save
-    end
-    @@all.last
+    @name = name
+    @songs = []
   end
 
   def songs
     @songs
   end
 
+  def self.all
+    @@all
+  end
+
+  def add_song(song)
+    @songs << song
+  end
+
   def save
     @@all << self
   end
 
-  def print_songs
-    @songs.each do |s|
-      puts s.name
+  def self.find_or_create_by_name(name)
+    artist = @@all.find {|artist| artist.name == name}
+    if artist == nil
+      artist = Artist.new(name)
+      artist.save
     end
+    @@all.last
   end
+
+  def print_songs
+    @songs.each{|song| puts "#{song}"}
+  end
+
 end
- ##add_song
-    #takes in an argument of a song and associates that song with the artist by telling the song that it belongs to that artist
