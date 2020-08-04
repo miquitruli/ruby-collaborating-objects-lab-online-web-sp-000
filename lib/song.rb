2405b2aaf2b7586bc83b4585fa9467306c97c060
MICAELLA COLOMBO <micaelc@g.clemson.edu>
2020-08-04 00:53:07 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,8 +1,14 @@
 class Song
   attr_accessor :name, :artist
+  @@all =[]
+
+  def self.all
+    @@all
+  end
 
   def initialize(name)
     @name=name
+    @@all << self
   end
 
   def artist_name=(name)
